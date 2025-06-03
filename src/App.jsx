2cdda620da8294796eca019eb7be54352c603e77
willysafari <willymkumbwa@gmail.com>
2025-06-03 17:58:06 +0300

    JSON FILE ADDED TO FIX VERCEL ERROR ON BROWSER

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Routes, Route , Navigate } from "react-router-dom";
 import Home from "./views/Home";
 import Indregients from "./views/Indregients";
 import MealsByIngredient from "./views/MealsByIngredient";
 import MealDetail from "./views/MealDetail"; 
+import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
 // Import the new MealDetail component
 
 function App() {  
